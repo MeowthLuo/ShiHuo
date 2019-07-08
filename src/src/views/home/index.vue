5b426df799f1fd0ff124c6ab52cd646b13a099fb
MeowthLuo <903252078@qq.com>
2019-07-08 09:20:39 +0800

    luotijiao

---
+++ src/src/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>  
+        <Home></Home>
+</template>
+
+<script>
+import Home from "components/home/home.vue"
+export default {
+    name:"home",
+    components:{
+        Home
+    },
+}
+</script>
+
+<style>
+
+</style>

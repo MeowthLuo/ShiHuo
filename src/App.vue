687959ae2aaaea70f1575a65629207a1bc90266e
平远 <1026892913@qq.com>
2019-07-08 09:04:14 +0800

    12346

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import Footer from "common/footer/footer.vue"
+export default {
+  name: 'app',
+  components:{
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>
